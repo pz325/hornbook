51f2f819e8446bc7c9c9797c40ba2ad7c4de436b
Ping Zou <sg71.cherub@gmail.com>
2015-09-16 21:56:39 +0100

    rename application to hornbook

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ from setuptools import setup
 
 setup(
     # GETTING-STARTED: set your app name:
-    name='YourAppName',
+    name='hornbook',
     # GETTING-STARTED: set your app version:
     version='1.0',
     # GETTING-STARTED: set your app description:
-    description='OpenShift App',
+    description='Chinese flash card application',
     # GETTING-STARTED: set author name (your name):
-    author='Your Name',
+    author='Ping.ZOU',
     # GETTING-STARTED: set author email (your email):
-    author_email='example@example.com',
+    author_email='sg71.cherub@gmail.com',
     # GETTING-STARTED: set author url (your url):
-    url='http://www.python.org/sigs/distutils-sig/',
+    url='http://menrfa.wordpress.com',
     # GETTING-STARTED: define required django version:
     install_requires=[
         'Django==1.8.4'
--- wsgi/hornbook/hornbook/settings.py
+++ wsgi/hornbook/hornbook/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for myproject project.
+Django settings for hornbook project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/topics/settings/
@@ -60,8 +60,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-# GETTING-STARTED: change 'myproject' to your project name:
-ROOT_URLCONF = 'myproject.urls'
+ROOT_URLCONF = 'hornbook.urls'
 
 TEMPLATES = [
     {
@@ -79,7 +78,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.wsgi.application'
+WSGI_APPLICATION = 'hornbook.wsgi.application'
 
 
 # Database
