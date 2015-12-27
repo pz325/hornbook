f9ad291f38c8292a573f67009a940b901c9d4db6
Ping Zou <sg71.cherub@gmail.com>
2015-12-27 14:57:07 +0000

    add gulp config, also update package.json for necessary dependencies

---
+++ wsgi/gulpfile.js
@@ -0,0 +1,59 @@
+"use strict";
+
+var gulp = require('gulp');
+var browserify = require('browserify'); // Bundles JS
+var reactify = require('reactify');  // Transforms React JSX to JS
+var source = require('vinyl-source-stream'); // use conventional text streams with Gulp
+var concat = require('gulp-concat'); //concatenate files
+
+
+var config = {
+    port: 8000,
+    devBaseUrl: 'http://localhost',
+    paths: {
+        html: './static/*.html',
+        js: './static/**/*.js',
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+            './static/**/*.css'
+        ],
+        glyphicons: [
+            'node_modules/bootstrap/fonts/glyphicons-halflings-regular.eot',
+            'node_modules/bootstrap/fonts/glyphicons-halflings-regular.svg',
+            'node_modules/bootstrap/fonts/glyphicons-halflings-regular.ttf',
+            'node_modules/bootstrap/fonts/glyphicons-halflings-regular.woff',
+            'node_modules/bootstrap/fonts/glyphicons-halflings-regular.woff2'
+        ],
+        dist: './dist',
+        mainJs: './static/js/main.js'
+    }
+}
+
+gulp.task('js', function() {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/js'));
+});
+
+gulp.task('css', function(){
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
+gulp.task('glyphicons', function() {
+    gulp.src(config.paths.glyphicons)
+        .pipe(gulp.dest(config.paths.dist + '/fonts'));
+});
+
+gulp.task('watch', function() {
+    // gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
+    gulp.watch(config.paths.css, ['css']);
+});
+
+gulp.task('default', ['js', 'watch', 'css', 'glyphicons']);
