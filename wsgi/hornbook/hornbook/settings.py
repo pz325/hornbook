c83f4fe8f2da0bc407fc471613497784a69ec6ab
Ping Zou <sg71.cherub@gmail.com>
2016-01-02 10:54:52 +0000

    disable DEBUG in setting.py

--- wsgi/hornbook/hornbook/settings.py
+++ wsgi/hornbook/hornbook/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = SECRETS['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
-DEBUG = True
+# DEBUG = True
 
 from socket import gethostname
 ALLOWED_HOSTS = [
