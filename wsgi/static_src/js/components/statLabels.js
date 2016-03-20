4a10746ca7829d940bf78fa923670bb1e80123c5
Ping Zou <sg71.cherub@gmail.com>
2016-03-20 22:54:08 +0000

    further shorten the status display

--- wsgi/static_src/js/components/statLabels.js
+++ wsgi/static_src/js/components/statLabels.js
@@ -19,7 +19,7 @@ var StatLabels = React.createClass({
                 <ReactBootstrap.Label bsStyle='warning'>New: {this.props.stats.new}</ReactBootstrap.Label>
                 <ReactBootstrap.Label bsStyle='info'>Studying: {this.props.stats.studying}</ReactBootstrap.Label>
                 <ReactBootstrap.Label bsStyle='success'>Grasped: {this.props.stats.grasped}</ReactBootstrap.Label>
-                <ReactBootstrap.Label bsStyle='info'>Last Study: {lastStudyTimestamp.toDateString()}</ReactBootstrap.Label>
+                <ReactBootstrap.Label bsStyle='info'>Last: {lastStudyTimestamp.toDateString()}</ReactBootstrap.Label>
             </div>
         );
     }
